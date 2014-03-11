e467b8b2d515919ffb330b144b72a272c283cb70
kolesofortuni <kolesofortuni@mail.ru>
2014-03-11 20:31:07 +0400

    TP SEM

--- public/js/models/score.js
+++ public/js/models/score.js
@@ -5,13 +5,10 @@ define([
 ){
 
     var Model = Backbone.Model.extend({
-<<<<<<< HEAD
         defaults: {
         	name: '',
         	score: 0
         }
-=======
->>>>>>> tp/v4
     });
 
     return Model;
