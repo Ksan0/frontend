f2851cc4e745728e59f0c8819a0e0ddef633b410
krygin.ia <krygin.ia@gmail.com>
2014-03-31 23:29:09 +0400

    Scoreboard loading screen added

--- public/js/views/scoreboard.js
+++ public/js/views/scoreboard.js
@@ -1,31 +1,60 @@
 define([
     'backbone',
     'tmpl/scoreboard',
+    'tmpl/loading',
+    'tmpl/loading_failed',
     'collections/scores'
 ], function(
     Backbone,
     tmpl,
+    tmpl_loading,
+    tmpl_loading_failed,
     scores
 ){
     var View = Backbone.View.extend({
         template: tmpl,
+        template_loading: tmpl_loading,
+        template_loading_failed: tmpl_loading_failed,
         initialize: function () {
             this.$el.html(this.template({scores: scores.toJSON()}));
             $('.content_wrapper').append(this.$el);
             this.hide();
         },
-        render: function () {
-            console.log(scores.fetch);
-            scores.fetch({
-                data:{limit:10}
-            });
+        renderLoading: function () {
+            this.$el.html(this.template_loading());
+            console.log("ЗАГРУЗКА");
+            return this;
+        },
+        renderScoreboard: function () {
             this.$el.html(this.template({scores: scores.toJSON()}));
+            console.log("ТАБЛИЦА ЗАГРУЖЕНА");
+            return this;
+        },
+        renderError: function() {
+            this.$el.html(this.template_loading_failed());
+            console.log("ТАБЛИЦА НЕ ЗАГРУЖЕНА");
             return this;
         },
         show: function () {
-            this.render();
-            this.$el.show();
+            scores.fetch({
+                data:{limit:10},
+                success: this.showScoreboard.bind(this),
+                error: this.showErrorMessage.bind(this)
+            });
+            this.showLoading();
             this.trigger('show', this);
+        },
+        showScoreboard: function (collection, response, options) {
+            this.renderScoreboard();
+            this.$el.show();
+        },
+        showErrorMessage: function(collection, response, options) {
+            this.renderError();
+            this.$el.show();
+        },
+        showLoading: function () {
+            this.renderLoading();
+            this.$el.show();
         },
         hide: function () {
             this.$el.hide();
